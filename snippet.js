289e68c578285dc9f1cb245a740dc152b2263c0b
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-10-01 18:16:55 -0500

    added the snippet of code from coding trainer that has a regular expression

---
+++ snippet.js
@@ -0,0 +1,36 @@
+function perMinute() {
+
+	// adds a string with various keyboarding scores
+	let wpmString = 'WPM 450 | Correct 45 | Incorrect 23';
+	// adds a regular expression to search for the wpm
+	let regularExpression = new RegExp('[^0-9]*([0-9]+)', '');
+
+	// finds a match in the wpm string
+	if (wpmString.match(regularExpression)) {
+      
+		// adds the value of wpm using match() method
+		let wpmMatch = wpmString.match(regularExpression)
+			? wpmString.match(regularExpression)[1]
+			: 0; // value is zero if match() returns falsy
+
+		// logs the value from match() method
+		console.log(`let wpmMatch = ${wpmMatch}`);
+
+		return wpmMatch;
+
+	} else {
+
+		// this is for debugging purposes
+		console.log(`!wpmString.match(${regularExpression})`);
+		// returns a default value
+		return 0;
+
+	}
+
+}
+
+// todo: shows the log
+perMinute();
+
+// todo: shows the returned value
+// console.log(`perMinute() = ${perMinute()}`);
